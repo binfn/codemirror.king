58ecc7a2123759443d5b150b3a6f74f394f6a325
xuybin <xuybin@qq.com>
2022-03-30 20:37:48 +0800

    refactor:

--- lezer/lr/parse.ts
+++ lezer/lr/parse.ts
@@ -4,7 +4,7 @@ import {Stack, StackBufferCursor} from "./stack.ts"
 import {Action, Specialize, Term, Seq, StateFlag, ParseState, File} from "./constants.ts"
 import {Tokenizer, TokenGroup, ExternalTokenizer, CachedToken, InputStream} from "./token.ts"
 import {decodeArray} from "./decode.ts"
-import { process } from "https://deno.land/std@0.132.0/node/process.ts";
+import { process } from "https://cdn.deno.land/std/versions/0.132.0/raw/node/process.ts";
 
 //https://deno.land/std@0.132.0/node/process.ts
 //https://cdn.deno.land/std/versions/0.132.0/raw/node/process.ts
